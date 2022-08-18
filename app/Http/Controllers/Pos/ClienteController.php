81a0595cbc344308719c20be0ba00dc074a5c6f1
Manuel Ninahuanca <mninahuanca4@gmail.com>
2022-08-18 12:40:44 -0400

    Terminando el CRUD de cliente

--- app/Http/Controllers/Pos/ClienteController.php
+++ app/Http/Controllers/Pos/ClienteController.php
@@ -52,4 +52,81 @@ class ClienteController extends Controller
             ->route('cliente.all')
             ->with($notification);
     }
+
+    public function clienteEdit($id)
+    {
+        $cliente = Cliente::findOrFail($id);
+        return view('backend.cliente.cliente_edit', compact('cliente'));
+    }
+
+    public function clienteUpdate(Request $request)
+    {
+        $cliente_id = $request->id;
+        if ($request->file('imagen_cliente')) {
+            $image = $request->file('imagen_cliente');
+            $name_gen =
+                hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+            Image::make($image)
+                ->resize(200, 200)
+                ->save('upload/cliente/' . $name_gen);
+            $save_url = 'upload/cliente/' . $name_gen;
+
+            Cliente::findOrFail($cliente_id)->update([
+                'nombre' => $request->nombre,
+                'imagen_cliente' => $save_url,
+                'nit' => $request->nitcliente,
+                'telefono' => $request->telefono,
+                'email' => $request->email,
+                'direccion' => $request->direccion,
+                'updated_by' => Auth::user()->id,
+                'updated_at' => Carbon::now(),
+            ]);
+
+            $notification = [
+                'message' => 'Cliente modificado con exito',
+                'alert-type' => 'success',
+            ];
+
+            return redirect()
+                ->route('cliente.all')
+                ->with($notification);
+        } else {
+            Cliente::findOrFail($cliente_id)->update([
+                'nombre' => $request->nombre,
+                'nit' => $request->nitcliente,
+                'telefono' => $request->telefono,
+                'email' => $request->email,
+                'direccion' => $request->direccion,
+                'updated_by' => Auth::user()->id,
+                'updated_at' => Carbon::now(),
+            ]);
+
+            $notification = [
+                'message' => 'Cliente modificado con exito',
+                'alert-type' => 'success',
+            ];
+
+            return redirect()
+                ->route('cliente.all')
+                ->with($notification);
+        }
+    }
+
+    public function clienteDelete($id)
+    {
+        $cliente = Cliente::findOrFail($id);
+        $image = $cliente->imagen_cliente;
+        unlink($image);
+
+        Cliente::findOrFail($id)->delete();
+        
+        $notification = [
+            'message' => 'Cliente eliminado con exito',
+            'alert-type' => 'success',
+        ];
+
+        return redirect()
+            ->back()
+            ->with($notification);
+    }
 }
--- resources/views/backend/cliente/cliente_all.blade.php
+++ resources/views/backend/cliente/cliente_all.blade.php
@@ -19,57 +19,58 @@
                         <div class="card-body">
                             <a href="{{ route('cliente.add') }}" class="btn btn-dark btn-rounded">Nuevo Cliente</a>
                             <hr>
-
                             {{-- <h4 class="card-title">Todos los datos de proveedores </h4> --}}
 
-
                             <table id="datatable" class="table table-bordered dt-responsive nowrap"
-                                style="border-collapse: collapse; border-spacing: 0; width: 100%;">
-                                <thead>
+                            style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                            <thead>
+                                <tr>
+
+                                    <th>Sl</th>
+                                    <th>Foto</th>
+                                    <th>Nombre</th>
+                                    <th>NIT</th>
+                                    <th>Email</th>
+                                    <th>Telefono</th>
+                                    <th>Direccion</th>
+                                    <th>Acciones</th>
+
+                            </thead>
+
+
+                            <tbody>
+
+                                @foreach ($clientes as $key => $item)
                                     <tr>
+                                        <td> {{ $key + 1 }} </td>
+                                        <td> <img src="{{ asset($item->imagen_cliente) }}"
+                                                style="width:60px; height:60px"></td>
+                                        <td> {{ $item->nombre }} </td>
+                                        <td> {{ $item->nit }} </td>
+                                        <td> {{ $item->email }} </td>
+                                        <td> {{ $item->telefono }} </td>
+                                        <td> {{ $item->direccion }} </td>
+                                        >
 
-                                        <th>Id</th>
-                                        <th>Foto</th>
-                                        <th>Nombre</th>
-                                        <th>NIT</th>
-                                        <th>Email</th>
-                                        <th>Telefono</th>
-                                        <th>Direccion</th>
-                                        <th>Acciones</th>
+                                        <td>
+                                            <a href="{{ route('edit.cliente', $item->id) }}" class="btn btn-info sm"
+                                                title="Edit Data"> <i class="fas fa-edit"></i>
+                                            </a>
 
-                                </thead>
+                                            <a 
+                                            href="{{ route('delete.cliente', $item->id) }}" 
+                                                class="btn btn-danger sm" title="Delete Data"
+                                                id="delete"> <i class="fas fa-trash-alt"></i> </a>
 
+                                        </td>
 
-                                <tbody>
+                                    </tr>
+                                @endforeach
 
-                                    @foreach ($clientes as $key => $item)
-                                        <tr>
-                                            <td> {{ $key + 1 }} </td>
-                                            <td> <img src="{{ asset($item->imagen_cliente) }}"
-                                                    style="width:60px; height:60px"></td>
-                                            <td> {{ $item->nombre }} </td>
-                                            <td> {{ $item->nit }} </td>
-                                            <td> {{ $item->email }} </td>
-                                            <td> {{ $item->telefono }} </td>
-                                            <td> {{ $item->direccion }} </td>
-                                            >
+                            </tbody>
+                        </table>
 
-                                            <td>
-                                                <a {{-- href="{{ route('edit.proveedor', $item->id) }}" --}} class="btn btn-info sm" title="Edit Data"> <i
-                                                        class="fas fa-edit"></i>
-                                                </a>
-
-                                                <a {{-- href="{{ route('delete.proveedor', $item->id) }}" --}} class="btn btn-danger sm" title="Delete Data"
-                                                    id="delete"> <i class="fas fa-trash-alt"></i> </a>
-
-                                            </td>
-
-                                        </tr>
-                                    @endforeach
-
-                                </tbody>
-                            </table>
-
+                     
                         </div>
                     </div>
                 </div> <!-- end col -->
